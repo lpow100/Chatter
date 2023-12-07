c0c42ca045b1d728fb879eee194d8b3deef622b7
lpow100 <lgn.kocher@gmail.com>
2023-12-07 13:27:11 -0800

    Buffed main

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ from sys import stdout
 import os
 import pickle
 import platform
+os.mkdir(f"{os.environ['USERPROFILE']}/chatter")
 
 write = stdout.write
 def up(amount):
@@ -20,15 +21,22 @@ def load(filename):
     with open(filename,"rb") as file:
         return pickle.load(file)
 def clear():
-    if platform.platform() == "Windows":
-        os.system("clr")
+    if "Windows" in platform.platform():
+        os.system("cls")
     else:
         os.system("clear")
 
+pass_attempts = 0
+cmd_helps = {
+    "time":"prints the time",
+    "date":"prints the date",
+    "python":"runs the python interprter",
+    "echo":"prints what you put"
+}
 
 print("Hi I am chatter your chatbot!")
 try:
-    names = load("names.pk")
+    names = load(f"{os.environ['USERPROFILE']}/chatter/names.pk")
 except FileNotFoundError:
     names = {}
     
@@ -37,10 +45,30 @@ username = input(">").lower()
 if username in names.keys():
     print(f"Welcome {username}")
     while True:
+        if pass_attempts == 3:
+            print("You entered the wrong password too many times")
+            exit()
         print("Please enter your password")
         password_attempt = input(">").lower()
         if password_attempt == names[username]:
             break
         print("Wrong password please try again")
+        pass_attempts += 1
 else:
+    while True:
+        clear()
+        print("Enter your password")
+        password_attempt1 = input(">")
+        print("Confirm your password")
+        password_attempt2 = input(">")
+        if password_attempt1 == password_attempt2:
+            print("New user added")
+            names[username] = password_attempt1
+            break
     
+save(names,f"{os.environ['USERPROFILE']}/chatter/names.pk")
+while True:
+    cmd = input("$>")
+    if cmd == "help":
+        for x,y in zip(cmd_helps.keys(),cmd_helps.values()):
+            print(x,y)
