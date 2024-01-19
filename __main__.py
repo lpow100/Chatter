c5386cc3537fb40f7eb377c6af2a1adbd5d564ea
lpow100 <lgn.kocher@gmail.com>
2024-01-18 18:27:32 -0800

    uhh idk why i am owrking on this

--- __main__.py
+++ __main__.py
@@ -1,28 +1,19 @@
-try:
-    from rich import print as cprint
-    from sys import stdout
-    import sys
-    import os
-    import pickle
-    import platform
-    import datetime
-    import inquirer
-    from datetime import datetime
-    import getch
-except ModuleNotFoundError as e:
-    print("Could not use",e,"please add to pip")
-    exit()
+from rich import print as cprint
+from sys import stdout
+import sys
+import os
+import pickle
+import platform
+import datetime
+import inquirer
+from datetime import datetime
+import getch
 try:
     os.mkdir(f"{os.environ['USERPROFILE']}/chatter")
 except FileExistsError:
     pass
 
 write = stdout.write
-class CmdError(Exception):
-    """When the command is not valid."""
-    
-    def __init__(self, cmd) -> None:
-        super().__init__(f'Invalid Command "{cmd}"\n"{cmd}" is not a vaild command try help.')
 def up(amount):
     write(f"\u001b[{amount}A")
 def right(amount):
@@ -66,69 +57,22 @@ def date_maker()->str:
         else:
             del forms[forms.index(date1)]
             messsage.append(date1)
-        if i > 3:
+        if i < 3:
             up(len(forms)+3)
-        
-        def __init__(self, cmd) -> None:
-            super().__init__(f'Invalid Command "{cmd}"\n"{cmd}" is not a vaild command try help.')
-    def up(amount):
-        write(f"\u001b[{amount}A")
-    def right(amount):
-        write(f"\u001b[{amount}C")
-    def down(amount):
-        up(-amount)
-    def left(amount):
-        right(-amount)
-    def save(data, filename):
-        with open(filename,"wb") as file:
-            pickle.dump(data,file)
-    def load(filename):
-        with open(filename,"rb") as file:
-            return pickle.load(file)
-    def clear():
-        if "Windows" in platform.platform():
-            os.system("cls")
-        else:
-            os.system("clear")
-    def choose(item_name:str,message:str,items:list)->str:
-
-        questions = [
-            inquirer.List(
-                item_name,
-                message=message,
-                choices=items,
-            ),
-        ]
-        username = inquirer.prompt(questions)
-        return username[item_name] 
-    def date_maker()->str:
-        forms = ["DD","MM","YY","YYYY"]
-        messsage = []
-        for i in range(3):
-            date1 = choose("date1","What do you want first: ",forms)
-            messsage
-            if date1 in ("YY","YYYY"):
-                del forms[forms.index("YY")]
-                del forms[forms.index("YYYY")]
-                messsage.append(date1)
-            else:
-                del forms[forms.index(date1)]
-                messsage.append(date1)
-            up(len(forms)+2)
             
-        date_list = []
-        for part in messsage:
-            if part == "DD":
-                date_list.append("%d")
-            elif part == "MM":
-                date_list.append("%m")
-            elif part == "YY":
-                date_list.append("%y")
-            elif part == "YYYY":
-                date_list.append("%Y")
-        return input("Chose the seperator: ").join(date_list)
+    date_list = []
+    for part in messsage:
+        if part == "DD":
+            date_list.append("%d")
+        elif part == "MM":
+            date_list.append("%m")
+        elif part == "YY":
+            date_list.append("%y")
+        elif part == "YYYY":
+            date_list.append("%Y")
+    return input("Chose the seperator: ").join(date_list)
         
-    def make_cmd(cmds)->list[str]:
+def make_cmd(cmds)->list[str]:
         made_cmds = []
         cur = ""
         cont_times = 0
@@ -151,7 +95,7 @@ def date_maker()->str:
                 cur = ""
             cur += cmd
         if is_str:
-            raise CmdError(cmds)
+            cprint(f"[#B00020]Error: {cmds}\nIs missing quotes please use help for help[#B00020]",file=sys.stderr)
         return [cur] if made_cmds == [] else made_cmds
 
 pass_attempts = 0
@@ -200,47 +144,7 @@ else:
     
 save(names,f"{os.environ['USERPROFILE']}/chatter/names.pk")
 while True:
-    date = datetime.now()
-    cmd = make_cmd(input("$>"))
-    print(cmd)
-    if cmd[0] == "help":
-        for x,y in zip(cmd_helps.keys(),cmd_helps.values()):
-            print(x,y)
-    elif cmd[0] == "exit":
-        break
-    elif cmd[0] == "echo":
-        print(cmd[5:])
-    elif cmd[0] == "time":
-        print(date.strftime(time_form))
-    elif cmd[0] == "strftime":
-        print(date.strftime(cmd[2:]))
-    elif cmd[0] == "date":
-        print(date.strftime(date_form))
-    elif cmd[0] == "settime":
-        messsage = choose("Times","What format do you want to see the time?",
-                          ["12:59 pm","24:59","12:59 59 pm"])
-        if messsage == "12:59 pm":
-            time_form = "%l:%M %P"
-        elif messsage == "24:59":
-            time_form = "%H:%M"
-        elif messsage == "12:59 59 pm":
-            time_form = "%l:%M %S %p"
-    elif cmd[0] == "setdate":
-        date_form = date_maker()
-    else:
-        while True:
-            clear()
-            print("Enter your password")
-            password_attempt1 = input(">")
-            print("Confirm your password")
-            password_attempt2 = input(">")
-            if password_attempt1 == password_attempt2:
-                print("New user added")
-                names[username] = password_attempt1
-                break
-        
-    save(names,f"{os.environ['USERPROFILE']}/chatter/names.pk")
-    while True:
+    try:
         date = datetime.now()
         cmd = make_cmd(input("$>"))
         print(cmd)
@@ -261,22 +165,14 @@ while True:
             messsage = choose("Times","What format do you want to see the time?",
                             ["12:59 pm","24:59","12:59 59 pm"])
             if messsage == "12:59 pm":
-                time_form = "%I:%M %P"
+                time_form = "%l:%M %P"
             elif messsage == "24:59":
                 time_form = "%H:%M"
             elif messsage == "12:59 59 pm":
-                time_form = "%I:%M %S %p"
-        elif cmd == "setdate":
-            messsage = choose("Dates","What format do you want to see the date?",
-                            ["12/31/09","","12:59 59 pm"])
-            if messsage == "12:59 pm":
-                time_form = "%I:%M %p"
-            elif messsage == "24:59":
-                time_form = "%H:%M"
-            elif messsage == "12:59 59 pm":
-                time_form = "%I:%M %S %p"
+                time_form = "%l:%M %S %p"
+        elif cmd[0] == "setdate":
+            date_form = date_maker()
         else:
-            cprint(f"[#B00020]Error: {cmd} is not a valid command please use help for help[#B00020]",file=sys.stderr)
-            
-except KeyboardInterrupt:
-    quit()
+            cprint(f"[#B00020]Error: {cmd}\nIs not a valid command please use help for help[#B00020]",file=sys.stderr)
+    except KeyboardInterrupt:
+        pass
